10993c5566fd89a8b72b028e89510827eb83e106
Slerxerz <slerxerz@gmail.com>
2024-09-23 20:53:07 +0545

    User Menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [active,setActive]= useState(false)
+    const [open,setOpen]= useState(false)
 
     const isActive=()=>{
         window.scrollY>0 ? setActive(true) : setActive(false)
@@ -35,9 +36,25 @@ const Navbar = () => {
                 <span>Business</span>
                 <span>Explore</span>
                 <span>English</span>
-                <span>Sign In</span>
-                <span>Become a Seller</span>
-                <button>Join</button>
+                {!currentUser&&<span>Sign In</span>}
+                {!currentUser?.isSeller && <span>Become a Seller</span>}
+                {!currentUser&&<button>Join</button>}
+                {currentUser&& <div className="user" onClick={()=>setOpen(!open)}>
+                    <img src="https://fastly.picsum.photos/id/58/1280/853.jpg?hmac=YO3QnOm9TpyM5DqsJjoM4CHg8oIq4cMWLpd9ALoP908" alt="" />
+                    <span>{currentUser?.username}</span>
+                    {open && <div className="options">
+                        {currentUser?.isSeller && (
+                                <>
+                                    <span>Services</span>
+                                    <span>Add new Service</span>
+                                </>
+                            )
+                        }
+                        <span>Orders</span>
+                        <span>Messages</span>
+                        <span>Logout</span>
+                    </div>}
+                </div>} 
             </div>
             
         </div>
