cf9407ef80de3b0e326ddb3ac5be51adeeeafc05
Slerxerz <slerxerz@gmail.com>
2024-10-21 17:41:56 +0545

    Added api for adding new service

--- backend/routes/serviceRoute.js
+++ backend/routes/serviceRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { test } from '../controllers/serviceController.js';
+import { createService,deleteService,getService,getServices } from '../controllers/serviceController.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
 
-router.get('/test', test);
+router.post('/', verifyToken,createService);
+router.delete('/:id', verifyToken,deleteService);
+router.get('/single/:id', verifyToken,getService);
+router.get('/', verifyToken,getServices);
 
 export default router
